9dd01765fc3d1149045925e9ddfe9c22c8650b47
Donald Brunais <donb1991@hotmail.com>
2015-09-21 11:47:51 -0700

    required middleware

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+app = express();
+var bodyParser = require('body-parser');
+var session = require('cookie-session');
+var ejs = require('ejs');
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+
+app.use(bodyParser);
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(session({
+  maxAge: 36000000,
+  secret: 'Its on sale',
+  name: 'saleWatcher',
+}));
+
+app.listen(3000, function() {
+  console.log('http://localhost:3000');
+});
