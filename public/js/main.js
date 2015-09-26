c80f30eaa82281128e669429e8136d2f5e3c4ddb
Donald Brunais <donb1991@hotmail.com>
2015-09-26 11:11:16 -0700

    more styling improvements

--- public/js/main.js
+++ public/js/main.js
@@ -38,15 +38,15 @@ $(document).ready(function() {
     $results.html('');
     results.forEach(function(result) {
       var $row = $('<div class=\'row \'></div>');
-      var $column = $('<div class=\'columns large-12\'></div>');
+      var $column = $('<div class=\'columns medium-12\'></div>');
       var $result = $('<div class=\'game\'> </div>');
-      var $form = $('<div class=\'right columns large-4 vcenter\'> ' +
+      var $form = $('<div class=\'right columns medium-4 small-6 vcenter\'> ' +
         '<form class=\'\'action=\'#\' method=\'get\'>' +
           '<div class=\'row collapse\'>' +
-            '<div class=\'columns large-11 \'>' +
+            '<div class=\'columns small-11 \'>' +
               '<input type=\'Number\'class=\'userPrice\' placeholder=\'Your price\'></input>' +
             '</div>' +
-            '<div class=\'columns large-1\'>' +
+            '<div class=\'columns small-1\'>' +
               '<button class=\'button postfix success expand\' type="sumbit" name="button" disabled=true>+</button>' +
             '</div>' +
           '</div>' +
@@ -64,10 +64,16 @@ $(document).ready(function() {
         var game = {};
         game.title = result.external;
         game.gameId = result.gameID;
-        game.thumb = result.thumb;
         game.price = deal.gameInfo.salePrice;
         game.publisher = deal.gameInfo.publisher;
-        getRetailer(game, deal.gameInfo.storeID, $result);
+        $.ajax({
+          url:'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=' + game.title + ' Cover Art',
+          method: 'GET',
+          dataType: 'jsonp',
+        }).done(function(data) {
+          game.thumb = data.responseData.results[0].unescapedUrl;
+          getRetailer(game, deal.gameInfo.storeID, $result);
+        });
       });
     });
   }
@@ -88,8 +94,8 @@ $(document).ready(function() {
   }
 
   function addHiddenGameData(game, $result) {
-    var $img = $('<img class="coverArt columns large-2 vcenter" src="' + game.thumb + '" alt="' + game.title + ' Cover Art" />');
-    var $div = $('<div class="columns large-5 vcenter"> </div>');
+    var $img = $('<img class="coverArt columns medium-2 show-for-medium-up vcenter" src="' + game.thumb + '" alt="' + game.title + ' Cover Art" />');
+    var $div = $('<div class="columns medium-5 small-6 vcenter"> </div>');
     var $form = $result.find('form');
     $div.append('<span class="title">Title: ' + game.title + '</span><br />');
     $div.append('<span class="title">Retailer: ' + game.retailer + '</span> <br />');
@@ -173,4 +179,91 @@ $(document).ready(function() {
       $signupbutton.attr('disabled', false);
     }
   }
+
+  function googleImg() {
+    google.load('search', '1');
+
+    var imageSearch;
+
+    function addPaginationLinks() {
+
+      // To paginate search results, use the cursor function.
+      var cursor = imageSearch.cursor;
+      var curPage = cursor.currentPageIndex; // check what page the app is on
+      var pagesDiv = document.createElement('div');
+      for (var i = 0; i < cursor.pages.length; i++) {
+        var page = cursor.pages[i];
+        if (curPage == i) {
+          // If we are on the current page, then don't make a link.
+          var label = document.createTextNode(' ' + page.label + ' ');
+          pagesDiv.appendChild(label);
+        } else {
+          // Create links to other pages using gotoPage() on the searcher.
+          var link = document.createElement('a');
+          link.href = '/image-search/v1/javascript:imageSearch.gotoPage(' + i + ');';
+          link.innerHTML = page.label;
+          link.style.marginRight = '2px';
+          pagesDiv.appendChild(link);
+        }
+      }
+
+      var contentDiv = document.getElementById('content');
+      contentDiv.appendChild(pagesDiv);
+    }
+
+    function searchComplete() {
+
+      // Check that we got results
+      if (imageSearch.results && imageSearch.results.length > 0) {
+
+        // Grab our content div, clear it.
+        var contentDiv = document.getElementById('content');
+        contentDiv.innerHTML = '';
+
+        // Loop through our results, printing them to the page.
+        var results = imageSearch.results;
+        for (var i = 0; i < results.length; i++) {
+          // For each result write it's title and image to the screen
+          var result = results[i];
+          var imgContainer = document.createElement('div');
+          var title = document.createElement('div');
+
+          // We use titleNoFormatting so that no HTML tags are left in the
+          // title
+          title.innerHTML = result.titleNoFormatting;
+          var newImg = document.createElement('img');
+
+          // There is also a result.url property which has the escaped version
+          newImg.src = '/image-search/v1/result.tbUrl;';
+          imgContainer.appendChild(title);
+          imgContainer.appendChild(newImg);
+
+          // Put our title + image in the content
+          contentDiv.appendChild(imgContainer);
+        }
+
+        // Now add links to additional pages of search results.
+        addPaginationLinks(imageSearch);
+      }
+    }
+
+    function OnLoad() {
+
+      // Create an Image Search instance.
+      imageSearch = new google.search.ImageSearch();
+
+      // Set searchComplete as the callback function when a search is
+      // complete.  The imageSearch object will have results in it.
+      imageSearch.setSearchCompleteCallback(this, searchComplete, null);
+
+      // Find me a beautiful car.
+      imageSearch.execute('Subaru STI');
+
+      // Include the required Google branding
+      google.search.Search.getBranding('branding');
+    }
+
+    google.setOnLoadCallback(OnLoad);
+  }
+
 });
